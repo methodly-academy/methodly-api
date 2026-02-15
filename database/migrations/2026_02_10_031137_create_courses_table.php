c831400cf7250889d028aaaec6b1f84df6a5295b
syrf27 <asahclass@gmail.com>
2026-02-15 09:30:32 +0700

    Revert "chore: migration and model for Course, Chapter, Lesson, Level and Series"
    This reverts commit e64b6262d5c53e5dc289092fb8f411665550e4f6.

--- database/migrations/2026_02_10_031137_create_courses_table.php
+++ database/migrations/2026_02_10_031137_create_courses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name',100);
+            $table->string('title',100);
             $table->string('slug',100)->unique();
             $table->longText('description')->nullable();
             $table->string('thumbnail')->nullable();
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->integer('price')->default(0)->nullable();
             $table->boolean('is_published')->default(false);
             // Jika level dihapus, tabel course tetap masih ada
-            $table->foreignId('level_id')->constrained('levels')->onDelete('cascade');
+            $table->foreignId('level_id')->constrained('levels')->nullOnDelete();
             $table->timestamps();
         });
     }
